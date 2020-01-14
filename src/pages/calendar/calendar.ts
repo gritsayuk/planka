ad4fdf75b9a87f9eec8e8b2e820b68cc690e494a
sergeygritsayuk <s.gritsayuk@gmail.com>
2020-01-14 18:41:11 +0200

    11111

--- src/pages/calendar/calendar.ts
+++ src/pages/calendar/calendar.ts
@@ -1,24 +1,39 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { CalendarComponentOptions } from 'ion2-calendar';
-
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'page-calendar',
   templateUrl: 'calendar.html',
 })
 export class CalendarPage {
-  optionsMulti: CalendarComponentOptions;
-  dateMulti: string[];
-  type: 'string'; // 'string' | 'js-date' | 'moment' | 'time' | 'object'
-constructor(public navCtrl: NavController, public navParams: NavParams) {
-  this.optionsMulti = {
-    pickMode: 'multi',
-    monthPickerFormat: ['Январь', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'],
-    daysConfig: [{date: new Date(2020, 0, 5), cssClass: 'good-day'},
-    {date: new Date(2020, 0, 5), cssClass: 'good-day'}]
-  };
+  optionsMulti : CalendarComponentOptions;
+  dateMulti : string[];
+  type : 'string'; // 'string' | 'js-date' | 'moment' | 'time' | 'object'
+  history : any = {};
+constructor(public navCtrl: NavController, public navParams: NavParams, private storage: Storage) {
+  this.initCalendar();
   }
+  initCalendar() {
+    let startDT : Date = new Date();
+    let dtsJSON : any = [];
+    this.storage.get("history")
+    .then(res => {
+      if (!!res) {
+        this.history = res;
+        console.log(">>>>",this.history);
+        for (var i in this.history) {
+          dtsJSON.push({date: new Date(1*i), cssClass: 'good-day'});
+        }
+      }
+      this.optionsMulti = {
+        pickMode: 'multi',
+        monthPickerFormat: ['Январь', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'],
+        daysConfig: dtsJSON
+      };
+        });
+}
   onChange($event) {
     console.log(">>>>onChange>>>>",$event);
     console.log(">>>>dateMulti>>>>",this.dateMulti);
