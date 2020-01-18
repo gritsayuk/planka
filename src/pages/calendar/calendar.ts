ebb116af3c2f9e9436495c3deb4a0f0b805d1094
chipman <gsvserg@ukr.net>
2020-01-19 01:32:39 +0200

    133

--- src/pages/calendar/calendar.ts
+++ src/pages/calendar/calendar.ts
@@ -37,9 +37,9 @@ constructor(public navCtrl: NavController,
         //console.log(">>>>",this.history);
         for (var i in this.history) {
           if (this.fromDate == 0) {
-            this.fromDate = new Date(1*i); 
+            this.fromDate = new Date(parseInt(i)); 
           }
-          dtsJSON.push({date: new Date(1*i), cssClass: 'good-day'});
+          dtsJSON.push({date: new Date(parseInt(i)), cssClass: 'good-day'});
         }
       }
       this.optionsMulti = {
@@ -47,15 +47,39 @@ constructor(public navCtrl: NavController,
         this.fromDate,
         pickMode: 'multi',
         monthPickerFormat: this.transtateList.monthPickerFormat.split(','),
-        monthFormat: 'MMMM YYYY',
+        weekdays: this.transtateList.weekdaysPickerFormat.split(','),
+        //monthFormat: 'MMMM YYYY',
+        monthFormat: 'MM-YYYY',
         daysConfig: dtsJSON
       };
         });
 }
   onChange($event) {
-    //console.log(">>>>onChange>>>>",$event);
-    //console.log(">>>>dateMulti>>>>",this.dateMulti);
-    
+    let allTime = parseInt(this.history[$event[0]["_i"]].AllTimeOK)/1000;
+    let timeH = 0;
+    let timeM = 0;
+    let allTimeStr = "";
+
+    if(allTime >= 3600) {
+      timeH = Math.trunc(allTime/3600);
+      allTime -= timeH*3600; 
+      allTimeStr = timeH.toString() + this.transtateList.h
+    }
+
+    if(allTime >= 60) {
+      timeM = Math.trunc(allTime/60);
+      allTime -= timeM*60; 
+      allTimeStr += allTimeStr == "" ? "" : " ";
+      allTimeStr += timeM.toString() + this.transtateList.min
+    }
+
+    if(allTime > 0) {
+      allTimeStr += allTimeStr == "" ? "" : " ";
+      allTimeStr += allTime.toString() + this.transtateList.sek
+    }
+
+    alert(this.transtateList.long + allTimeStr);
+
   }
   ionViewDidLoad() {
     //console.log('ionViewDidLoad CalendarPage');
